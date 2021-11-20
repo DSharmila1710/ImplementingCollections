9b1fe5f3296216fd4dacfd74bf18639eec3062fa
DSharmila1710 <56862689+DSharmila1710@users.noreply.github.com>
2021-11-20 09:45:10 +0530

    Program to find min element in a stack using Node

---
+++ StackMinUsingNode.java
@@ -0,0 +1,63 @@
+package test2;
+import java.util.*;
+
+
+class StackMinUsingNode {
+
+	Stack<Node> s;
+	
+	class Node {
+		int value;
+		int min;
+		public Node(int value, int min) {
+			this.value=value;
+			this.min=min;
+		}
+	}
+
+	public StackMinUsingNode() {
+		this.s = new Stack<Node>();
+	}
+	
+	public int pop() {
+		return this.s.pop().value;
+	}
+	
+	public int peek() {
+		return this.s.peek().value;
+	}
+	
+	public int getMin() {
+		return this.s.peek().min;
+	}
+	
+	public void push(int i) {
+		System.out.println(i+" is inserted");
+		if(s.isEmpty()) {
+			s.push(new Node(i,i));
+		}else {			
+			s.push(new Node(i,Math.min(i, this.s.peek().min)));
+		}
+	}
+	
+	public static void main(String[] args) {
+		StackMinUsingNode stack = new StackMinUsingNode();
+		stack.push(4);
+		System.out.println(stack.getMin());
+		
+		stack.push(3);
+		System.out.println(stack.getMin());
+		
+		stack.push(5);
+		System.out.println(stack.getMin());
+		
+		stack.peek();
+		System.out.println(stack.getMin());
+		
+		stack.pop();
+		System.out.println(stack.getMin());
+		
+		stack.pop();
+		System.out.println(stack.getMin());
+	}
+}
